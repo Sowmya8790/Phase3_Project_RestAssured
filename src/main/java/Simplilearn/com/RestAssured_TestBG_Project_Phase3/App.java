089d0cac835d3fa0230f859a8aaa101abb71977b
Sowmya8790 <sowmi3v@gmail.com>
2022-12-11 12:39:54 +0530

    irst commit

---
+++ src/main/java/Simplilearn/com/RestAssured_TestBG_Project_Phase3/App.java
@@ -0,0 +1,13 @@
+package Simplilearn.com.RestAssured_TestBG_Project_Phase3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
